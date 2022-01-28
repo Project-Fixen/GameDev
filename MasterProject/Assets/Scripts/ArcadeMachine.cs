06aad257174d302407acd96ebec48c0bac8fc505
Project-Fixen <projectfixen@gmail.com>
2022-01-28 09:16:37 -0700

    Added the Current stage of the game
    Pushed current game (With stolen code oops)

---
+++ MasterProject/Assets/Scripts/ArcadeMachine.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcadeMachine : MonoBehaviour
+{
+    public GameObject Game;
+
+    void OnInteract() {
+        Debug.Log("Hit");
+    }
+}
